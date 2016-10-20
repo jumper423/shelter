fa54fd7abcdd0ae6dc3bae5d9b43f9ce5f4123c3
jumper423 <jump.e.r@yandex.ru>
2016-10-20 20:57:28 +0300

    view animal

--- src/Shelter.php
+++ src/Shelter.php
@@ -5,19 +5,19 @@ namespace jumper423;
 class Shelter
 {
     private static $instance = null;
+
     /**
      * @return Shelter
      */
     public static function getInstance() : Shelter
     {
-        if (null === self::$instance)
-        {
+        if (null === self::$instance) {
             self::$instance = new self();
         }
         return self::$instance;
     }
-    private function __clone() {}
-    private function __construct() {}
+    private function __clone(){}
+    private function __construct(){}
 
     /**
      * @var Animal[]
@@ -27,9 +27,25 @@ class Shelter
     /**
      * @param Animal $animal
      */
-    public function newAnimal(Animal $animal) {
+    public function newAnimal(Animal $animal)
+    {
         $animal->setAge(rand(1, 12));
         $animal->setNickname(substr(md5(rand(1, 1000) . time()), 0, 10));
         $this->animals[] = $animal;
+    }
+
+    /**
+     * @param Animal $type
+     * @return Animal[]
+     */
+    public function viewAnimal(Animal $type)
+    {
+        $animals = array_filter($this->animals, function (Animal $animal) use ($type) {
+            return $animal instanceof $type;
+        });
+        uasort($animals, function (Animal $animal1, Animal $animal2) {
+            return $animal1->getNickname() <=> $animal2->getNickname();
+        });
+        return $animals;
     }
 }
